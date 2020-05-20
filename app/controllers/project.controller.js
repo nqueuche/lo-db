f2fdfa560489cc5bce4b67bfd70f90c9e9794543
Nina <n.queuche@gmail.com>
2020-05-20 16:54:52 +0200

    add projects controller

---
+++ app/controllers/project.controller.js
@@ -0,0 +1,38 @@
+const db = require("../models");
+const Tutorial = db.projects;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new project
+exports.create = (req, res) => {
+  
+};
+
+// Retrieve all projects from the database.
+exports.findAll = (req, res) => {
+  
+};
+
+// Find a single project with an id
+exports.findOne = (req, res) => {
+  
+};
+
+// Update a project by the id in the request
+exports.update = (req, res) => {
+  
+};
+
+// Delete a project with the specified id in the request
+exports.delete = (req, res) => {
+  
+};
+
+// Delete all projects from the database.
+exports.deleteAll = (req, res) => {
+  
+};
+
+// Find all published projects
+exports.findAllPublished = (req, res) => {
+  
+};
